6f6cb602541e93b5ce50508f3c551aa6281b9408
syzio21 <shubhamsaurabhk@gmail.com>
2022-12-26 10:01:38 +0530

    day1

---
+++ Day1.js
@@ -0,0 +1,37 @@
+console.log("Masai School");
+console.log("A Transformation in Education")
+
+var name = "shubham";
+console.log("name")
+var name = "XYX";
+console.log(name)
+var name = "yzx";
+console.log("name")
+
+var name = "shubham";
+var age = 21;
+console.log("name", age)
+console.log(typeof("name"), typeof(age))
+
+let a = [
+  {
+    "name": "shubham",
+    "grade": "18",
+    "section": "a",
+    "rollno": "21"
+  },
+  {
+    "name": "y",
+    "grade": "18",
+    "section": "a",
+    "rollno": "21"
+  },
+  {
+    "name": "z",
+    "grade": "18",
+    "section": "a",
+    "rollno": "21"
+  }
+]
+
+console.log(a)
